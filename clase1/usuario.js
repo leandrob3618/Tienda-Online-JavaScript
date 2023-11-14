e2e9c40e4760f8894b7952a817d65d82e945ca19
Leandro Barrionuevo <leandrobarrionuevo20@gmail.com>
2023-11-14 15:34:22 -0300

    Modificación

---
+++ clase1/usuario.js
@@ -0,0 +1,40 @@
+
+
+//Bienvenida
+
+let mensaje;
+mensaje = 'Carreras de Trail REGISTRO'
+alert (mensaje);
+
+
+
+// Dato
+
+let nombreYapellido;
+nombreYapellido = prompt ('Ingrese Nombre y Apellido');
+alert (`Bienvenido ${nombreYapellido}!`);
+
+
+let fechadenacimiento
+fechadenacimiento = prompt ('Ingrese Fecha de Nacimiento')
+alert(fechadenacimiento)
+
+
+let contacto
+contacto = prompt ('Ingrese Numero de Telefono')
+alert(contacto)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
